66eb31134e5246d665b7ddec4269dcaef30388ef
Felix Neubauer <felix@neuby.de>
2025-04-18 10:53:09 +0200

    incorporate kenichi feedback

--- data-model/use_case_specific/sciformation_cleaner_mocof1.py
+++ data-model/use_case_specific/sciformation_cleaner_mocof1.py
@@ -52,9 +52,9 @@ def process_realization_text(data):
                 item['wait_after_rinse_unit'] = "h"
 
             # scCO2:
-            # If the realizationText contains "supercritical CO2" or "scCO2" or "scCO2" -> Add "scCO2" as wash_solid property.
+            # If the realizationText contains "scCO<sub>2</sub>" or "scCO2" or "scCO2" -> Add "scCO2" as wash_solid property.
             # If the realizationText contains "samples under fillers" or "MeOH filled up" -> Change "scCO2" into "MeOH+scCO2"
-            if any(x in realization_text.lower() for x in ["scCO<sub>2</sub>", "scco2"]):
+            if any(x in realization_text.lower() for x in ["scco<sub>2</sub>", "scco2"]):
                 item['wash_solid'] = "scCO2"
             if any(x in realization_text.lower() for x in ["samples under fillers", "meoh filled up"]):
                 item['wash_solid'] = "MeOH+scCO2"
